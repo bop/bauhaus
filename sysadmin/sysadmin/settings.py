2158a4f29756c42bcbf1df0487de92831649cfb8
bop <baptiste.ossipow@gmail.com>
2014-01-10 21:17:45 +0100

    ok hp

--- sysadmin/sysadmin/settings.py
+++ sysadmin/sysadmin/settings.py
@@ -131,6 +131,7 @@ INSTALLED_APPS = (
     'django.contrib.flatpages',
     'taggit',
     'sysadmin.blog',
+    'django_extensions',
 )
 
 # A sample logging configuration. The only tangible logging
