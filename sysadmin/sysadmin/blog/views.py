d0b21d19d650af6648c4f3fe3038603322c03711
bop <baptiste.ossipow@gmail.com>
2014-01-11 12:37:52 +0100

    home page

--- sysadmin/sysadmin/blog/views.py
+++ sysadmin/sysadmin/blog/views.py
@@ -6,4 +6,3 @@ from django.template import loader, Context
 def tagpage(request, tag):
     posts = Post.objects.filter(tags__name=tag)
     return render_to_response("blog/tagpage.html", {"posts":posts, "tag":tag})
-
