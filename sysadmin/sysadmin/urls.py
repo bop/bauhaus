f799557a8d43275b9d2001be3eef4359e9ba01df
bop <baptiste.ossipow@gmail.com>
2014-01-13 01:10:17 +0100

    -crct°

--- sysadmin/sysadmin/urls.py
+++ sysadmin/sysadmin/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'sysadmin.views.home', name='home'),
     # url(r'^sysadmin/', include('sysadmin.foo.urls')),
                        url(r'^$', include('sysadmin.foundation.urls')),
+                       url(r'^index.html/$', include('sysadmin.foundation.urls')),
                        url(r'^blog/', include('sysadmin.blog.urls')),
 
                        url(r'^admin/', include(admin.site.urls)),
